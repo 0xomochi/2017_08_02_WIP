62f9ff8897b6255d6efb012e79bfcd0e7ce6b893
yume <yume@sfc.wide.ad.jp>
2017-08-01 23:50:36 +0900

    Update log_killed_python.py

--- log_killed_python.py
+++ log_killed_python.py
@@ -3,10 +3,12 @@
 #なぜkillされたのかわからないのでとりあえずメモ
 
 #koneko_navy.exe
-
-
-
 >>> import pefile
 >>> pe = pefile.PE('/tmp/vector/koneko_navy.exe')
 >>> pe.parse_data_directories()
 zsh: killed     python
+  
+  
+#zipextractor.exe
+$ python auto.py non-malicious-file/zipextractor.exe                                                         []
+zsh: killed     python auto.py non-malicious-file/zipextractor.exe
