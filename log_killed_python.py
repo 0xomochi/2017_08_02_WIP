a2cba35060b9837f7bf81465e3e47836dddb06a8
yume <yume@sfc.wide.ad.jp>
2017-08-01 17:14:56 +0900

    Create log_killed_python.py

---
+++ log_killed_python.py
@@ -0,0 +1,10 @@
+#just a memo
+#killed pythonされたファイル
+#koneko_navy.exe
+
+
+
+>>> import pefile
+>>> pe = pefile.PE('/tmp/vector/koneko_navy.exe')
+>>> pe.parse_data_directories()
+zsh: killed     python
